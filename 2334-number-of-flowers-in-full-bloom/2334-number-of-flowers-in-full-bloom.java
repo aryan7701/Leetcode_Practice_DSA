735a469697d77a3a17c29f35488f8b70a1bde630
Aryan Agrawal <104223677+aryan7701@users.noreply.github.com>
2023-10-12 01:23:15 +0530

    Time: 22 ms (99.62%), Space: 72.1 MB (57.86%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.java
@@ -1,32 +1,71 @@
 class Solution {
-    public int[] fullBloomFlowers(int[][] flowers, int[] persons) {
-        int n = persons.length;
+    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        int m = flowers.length;
+        int[] starts = new int[m];
+        int[] ends = new int[m];
+        for(int i=0;i<m;i++)
+        {
+            starts[i]=flowers[i][0];
+            ends[i]= flowers[i][1];
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int n = people.length;
         int[] result = new int[n];
-        
-        // Create a TreeMap to track flower intervals and their frequencies.
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
-        for (int i = 0; i < flowers.length; i++) {
-            // Increment the start time frequency.
-            treeMap.put(flowers[i][0], treeMap.getOrDefault(flowers[i][0], 0) + 1);
-            // Decrement the end time frequency (add 1 to end time to indicate the end of the interval).
-            treeMap.put(flowers[i][1] + 1, treeMap.getOrDefault(flowers[i][1] + 1, 0) - 1);
-        }
-        
-        // Create another TreeMap to accumulate the frequencies and track the number of flowers blooming at each moment.
-        TreeMap<Integer, Integer> sum = new TreeMap<>();
-        int prev = 0;
-        for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
-            prev += entry.getValue();
-            sum.put(entry.getKey(), prev);
-        }
-        
-        // Calculate bloom statuses for each person.
-        for (int i = 0; i < n; i++) {
-            // Find the floor entry for the person's bloom time.
-            Map.Entry<Integer, Integer> entry = sum.floorEntry(persons[i]);
-            if (entry != null) {
-                result[i] = entry.getValue(); // Store the number of blooming flowers at that time.
+
+        for(int i=0;i<n;i++)
+        {
+            int open =0, close=0;
+            if(people[i]<starts[0])
+            {
+                open =0;
             }
+            else if(people[i]>=starts[m-1])
+            {
+                open = m;
+            }
+            else
+            {
+                int l =0, r=m-1;
+                while(l<r)
+                {
+                    int mid = (l+r+1)>>1;
+                    if(starts[mid]<=people[i])
+                    {
+                        l = mid;
+                    }
+                    else
+                    {
+                        r=mid-1;
+                    }
+                }
+                open = l+1;
+            }
+            if(people[i]<=ends[0]) 
+                close =0;
+            else if(people[i]>ends[m-1])
+            {
+                close =m;
+            }
+            else
+            {
+                int l=0, r=m-1;
+                while(l<r)
+                {
+                    int mid = (l+r+1)>>1;
+                    if(ends[mid]<people[i])
+                    {
+                        l=mid;
+
+                    }
+                    else
+                    {
+                        r = mid-1;
+                    }
+                }
+                close = l+1;
+            }
+            result[i]=open-close;
         }
         return result;
     }

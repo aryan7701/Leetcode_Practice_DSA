b23eb1a2c3cffaad09972690bf3f52867e782342
Aryan Agrawal <104223677+aryan7701@users.noreply.github.com>
2023-10-19 20:43:25 +0530

    Time: 69 ms (10.41%), Space: 41.7 MB (82.99%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -3,7 +3,12 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-const addTwoPromises = async function(promise1, promise2) {
- return Promise.all([promise1,promise2])
- .then(value => value.reduce((acc,val) => acc+val,0))
+var addTwoPromises = async function(promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    return value1+value2;
 };
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */

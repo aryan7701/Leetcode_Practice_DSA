555c1eaa933efa63b2b2f4d033a1fb197466c051
Aryan Agrawal <104223677+aryan7701@users.noreply.github.com>
2023-10-26 10:01:42 +0530

    Time: 0 ms (100%), Space: 43.9 MB (61.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int XorAll= nums.length;
+        int SumArr=0;
+        int n= nums.length;
         for(int i=0;i<nums.length;i++){
-            XorAll^=i^nums[i];
+            SumArr=SumArr+nums[i];
         }
-        return XorAll;
+        int Sum= n*(n+1)/2;
+        return Sum-SumArr;
     }
 }
